2cf20a6c44e898b164452ce7c7d5711c145f2037
Kris Ma <kriswjma@gmail.com>
2016-10-22 03:37:43 +0000

    boot

--- app/controllers/breathe_controller.rb
+++ app/controllers/breathe_controller.rb
@@ -1,12 +1,13 @@
 class BreatheController < ApplicationController
     
   def index
-    @users = User.all
-    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
-    marker.lat user.latitude
-    marker.lng user.longitude
-    end
-    @cities = session[:cities] || ['berkeley', 'fremont', 'irvine']
+    # @users = User.all
+    # @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+    # marker.lat user.latitude
+    # marker.lng user.longitude
+    # end
+    # @cityname = "Berkeley"
+    # @cities = session[:cities] || ['berkeley', 'fremont', 'irvine']
  end
     
 end
