1bae9511ec14b92da495bf7001a88575cae926d2
willtsay <willtsay@gmail.com>
2016-10-22 04:11:51 +0000

    got rid of test

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Getting Allergen and Weather Data" do
+
+    # this is the non ajax version subject to change!
+
+    # it "calls the model method that gets the location key from accuweather" do 
+    #   expect(City).to receive().with("Berkeley")
+    #   post :, {: => 'Berkeley'}
+    # end
+  
+  end
+
 end
