e86b535ecf98efa49577676a921a8814750bb313
Anish Khazane <akkhazan@berkeley.edu>
2016-10-22 01:51:00 +0000

    Implemented map and search

--- app/controllers/breathe_controller.rb
+++ app/controllers/breathe_controller.rb
@@ -1,6 +1,11 @@
 class BreatheController < ApplicationController
     
     def index
+    @users = User.all
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+    marker.lat user.latitude
+    marker.lng user.longitude
     end
+ end
     
 end
