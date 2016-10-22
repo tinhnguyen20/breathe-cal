276694d6657f41e0ad6314d2862af4a56e1df231
willtsay <willtsay@gmail.com>
2016-10-22 04:05:43 +0000

    ran rake db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021205626) do
+ActiveRecord::Schema.define(version: 20161022002722) do
 
   create_table "breathes", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 20161021205626) do
     t.text     "daily_data"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "address"
+    t.text     "description"
+    t.string   "title"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
