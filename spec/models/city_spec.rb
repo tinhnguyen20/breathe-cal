a710d9ee138eaaa7484b52a47c3a1a26fd66fa22
willtsay <willtsay@gmail.com>
2016-10-22 04:28:04 +0000

    got rid of tests with just pending

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,18 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  # describe "city#get_location_key" do
+  describe "city#get_location_key" do
 
-  #   it "gets the location key" do 
-  #     expect(City.get_location_key()).to eq 
-      
-  #   end
-  #   # this is the non ajax version subject to change!
+    it "gets the location key" do 
+      expect(City.get_location_key("94704", "Berkeley", "CA", "US")).to eq "39627_PC"
+    end
+    
+    
+    # this is the non ajax version subject to change!
 
-  #   #   expect(City).to receive().with("Berkeley")
-  #   # it "calls the model method that gets the location key from accuweather" do 
-  #   #   post :, {: => 'Berkeley'}
-  #   # end
-  # end
+    #   expect(City).to receive().with("Berkeley")
+    # it "calls the model method that gets the location key from accuweather" do 
+    #   post :, {: => 'Berkeley'}
+    # end
+  end
+  
+  describe "city#update_daily_data" do
+    
+    it "updates the daily_data" do
+      expect(true).to eq(true)      
+    end
+  
+  end
 
 end
