d8b62c6f948bf6686243a0297209ac23f37a4099
XIANGJIU Wang <wangxiangjiu1118@gmail.com>
2016-10-23 07:58:17 +0000

    adding the breathe california logo to assets as well as index

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20161022002722) do
 
-  create_table "breathes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "cities", force: :cascade do |t|
     t.string   "name"
     t.string   "state"
