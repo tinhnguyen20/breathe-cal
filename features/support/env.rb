58c1689c62f7a3679239c0394918e1f9be05483e
willtsay <willtsay@gmail.com>
2016-10-25 02:39:14 +0000

    added codeclimate env to travis.yml

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,13 +2,15 @@
 # It is recommended to regenerate this file in the future when you upgrade to a
 # newer version of cucumber-rails. Consider adding your own code to a new file
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
+
 require 'simplecov'
 SimpleCov.start do
   add_filter "/features/step_definitions/"
   add_filter "/features/support/"
 end
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+# require "codeclimate-test-reporter"
+# CodeClimate::TestReporter.start
+
 # files.
 require 'capybara/poltergeist'
 
