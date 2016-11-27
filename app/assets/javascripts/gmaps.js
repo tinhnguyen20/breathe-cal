800d7f604621e76575aff37cb4a6b14b41cf3c1b
willtsay <willtsay@gmail.com>
2016-11-27 07:14:06 +0000

    some changes not done yet

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -2,10 +2,12 @@
 // feature. People can enter geographical searches. The search box will return a
 // pick list containing a mix of places and predicted search terms.
 
+
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 var fetchedMarkers = [];
+
 
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -33,24 +35,30 @@ function initAutocomplete() {
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
+
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
     searchBox.setBounds(map.getBounds());
   });
 
+
   var markers = [];
+
 
   searchBox.addListener('places_changed', function() {
     var places = searchBox.getPlaces();
+
 
     if (places.length === 0) {
       return;
     }
 
+
     markers.forEach(function(marker) {
       marker.setMap(null);
     });
     markers = [];
+
 
     var bounds = new google.maps.LatLngBounds();
     places.forEach(function(place) {
@@ -65,6 +73,7 @@ function initAutocomplete() {
         anchor: new google.maps.Point(17, 34),
         scaledSize: new google.maps.Size(25, 25)
       };
+
 
       $.ajax({
         type: "POST",
@@ -85,6 +94,7 @@ function initAutocomplete() {
         position: place.geometry.location
       }));
 
+
       if (place.geometry.viewport) {
         bounds.union(place.geometry.viewport);
       }
@@ -99,12 +109,12 @@ function initAutocomplete() {
   var markerCount = 0;
   
   var uncommittedMarker;
-  
-  
+ 
   // allow user to put down a marker
   $("#marker-cta").click(function(){
     canMark = true;
   })
+
 
   google.maps.event.addListener(map, 'click', function(event) {
     if (canMark){
@@ -114,6 +124,21 @@ function initAutocomplete() {
   })
   
   var recentMarker;
+  
+  function createContentString(data){
+    var title = data.title;
+    var attributes = ["cat", "bee", "perfume", "oak", "peanut", "gluten", "dog", "dust", "smoke", "mold"];
+    var contentString ="<div>";
+    contentString += "Allergens at " + title + "<br>";
+    for(var i=0; i<attributes.length; i++){
+      if (data[attributes[i]]){
+        contentString += attributes[i] + "<br>";  
+      }
+    }
+    contentString += "</div>"
+    var content = $(contentString);
+    return content
+  }
   
   function placeMarker(location) {
     var marker = new google.maps.Marker({
@@ -125,9 +150,17 @@ function initAutocomplete() {
     
     var contentString = $(
       "<form id='markerForm' action='markers' method='POST'>"+
-      "<input type = 'checkbox' name='cat' value='true'> Cats Here <br>"+
-      "<input type = 'checkbox' name='dog' value='true'> Dogs Here <br>"+
-      "<input type = 'checkbox' name='mold' value='true'> Mold Here <br>"+
+      "Area Description <br> <input type='text' name='description'> <br>" + 
+      "<input type = 'checkbox' name='cat' value='true'> Cats <br>"+
+      "<input type = 'checkbox' name='bee' value='true'> Bees <br>"+
+      "<input type = 'checkbox' name='perfume' value='true'> Perfume <br>"+
+      "<input type = 'checkbox' name='oak' value='true'> Oak <br>"+
+      "<input type = 'checkbox' name='peanut' value='true'> Peanut <br>"+
+      "<input type = 'checkbox' name='gluten' value='true'> Gluten <br>"+
+      "<input type = 'checkbox' name='dog' value='true'> Dogs <br>"+
+      "<input type = 'checkbox' name='dust' value='true'> Dust <br>"+
+      "<input type = 'checkbox' name='smoke' value='true'> Smoke <br>"+
+      "<input type = 'checkbox' name='mold' value='true'> Mold <br>"+
       "<input type='submit' value='Submit'>"+
       "</form>"
     );
@@ -159,11 +192,11 @@ function initAutocomplete() {
         url: "markers",
         data: JSON.stringify({marker: convData}),
         success: function(d){
-          
-          var newContent = $("<div>"+
-                              "cat " + d.cat + 
-                              "<br> dog " + d.dog +
-                              "<br> mold " + d.mold + "</div>");
+          var newContent = createContentString(d);
+          // var newContent = $("<div>"+
+          //                     "cat " + d.cat + 
+          //                     "<br> dog " + d.dog +
+          //                     "<br> mold " + d.mold + "</div>");
           recentMarker.infowindow.setContent(newContent[0]);
           recentMarker.infowindow.open(map,recentMarker);
           recentMarker.draggable = false;
@@ -172,6 +205,8 @@ function initAutocomplete() {
       return false;
     });
   }
+  
+  // maybe just send a list of attributes to tell javascript to use....? 
   
   $("#breathe").unbind('click').bind('click', function(){
     var bounds = map.getBounds();
@@ -183,7 +218,7 @@ function initAutocomplete() {
       url: "markers",
       data: {bounds :{uplat:NECorner.lat(),downlat:SWCorner.lat(),rightlong:NECorner.lng(),leftlong:SWCorner.lng()}},
       success: function(data){
-        for(i=0;i<data.length; i++){
+        for(var i=0;i<data.length; i++){
           var location = {};
           location.lat = parseFloat(data[i].lat);
           location.lng = parseFloat(data[i].lng);
@@ -192,10 +227,11 @@ function initAutocomplete() {
                 map: map,
                 draggable: false,
                 });
-          var newContent = $("<div>"+
-                    "cat " + data[i].cat + 
-                    "<br> dog " + data[i].dog +
-                    "<br> mold " + data[i].mold + "</div>");
+          var newContent = createContentString(data[i]);      
+          // var newContent = $("<div>"+
+          //           "cat " + data[i].cat + 
+          //           "<br> dog " + data[i].dog +
+          //           "<br> mold " + data[i].mold + "</div>");
           marker.info = new google.maps.InfoWindow();
           marker.info.setContent(newContent[0]);
           google.maps.event.addListener(marker, 'click', function(){
@@ -207,7 +243,13 @@ function initAutocomplete() {
   })
 }
 
+
 $(document).ready(initAutocomplete);
 $(document).on('page:load', initAutocomplete);
 $(document).on('page:change', initAutocomplete);
+
+
+
+
+
 
--- db/migrate/20161117042638_create_markers.rb
+++ db/migrate/20161117042638_create_markers.rb
@@ -3,9 +3,17 @@ class CreateMarkers < ActiveRecord::Migration
     create_table :markers do |t|
       t.string :lng
       t.string :lat
+      t.string :title
       t.boolean :dog
       t.boolean :cat
       t.boolean :mold
+      t.boolean :bees
+      t.boolean :perfume
+      t.boolean :oak
+      t.boolean :dust
+      t.boolean :smoke
+      t.boolean :gluten
+      t.boolean :peanut
       t.timestamps null: false
     end
   end
