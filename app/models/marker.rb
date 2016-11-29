b7c959441e95816ffa5992a3a7e1e65fa0f488d1
willtsay <willtsay@gmail.com>
2016-11-29 01:21:46 +0000

    still some bugs to be fixed for label numbering

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -1,4 +1,6 @@
 class Marker < ActiveRecord::Base
+  belongs_to :client
+  
   
   def self.find_all_within_bounds(top, bottom, left, right)
     markersTop = Marker.where("lat < (?)", top)
@@ -7,7 +9,6 @@ class Marker < ActiveRecord::Base
     markersRight = Marker.where("lng > ?", right)
 
     between = markersBottom & markersTop & markersLeft & markersRight
-    puts between.as_json
     return between
     # markers = Marker.where("lng < (?) AND lat < (?) AND lng > (?) AND lat > (?) ", right, top, left, bottom)
   end
