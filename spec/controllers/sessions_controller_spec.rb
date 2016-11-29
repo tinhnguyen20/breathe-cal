e39e93c747575c5bcbe0cf06d255233ad3406ec9
willtsay <willtsay@gmail.com>
2016-11-29 03:50:54 +0000

    disable a rake

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,12 +2,4 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   
-  describe "standin" do
-    context "change this" do
-      it "pending" do
-        
-      end
-    end
-  end
-
 end
