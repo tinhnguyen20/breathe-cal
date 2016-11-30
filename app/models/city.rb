e7ccb9e19fd2b9b8f3306a6543ea40542dcb4f41
XIANGJIU Wang <wangxiangjiu1118@gmail.com>
2016-11-30 05:46:36 +0000

    api_working now with newly added api key and put in the front of the array

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,7 +3,7 @@ class City < ActiveRecord::Base
   serialize :daily_data, JSON
   
   def self.get_api_key(i)
-    ["IGE0pfTgoL1OGJKvEcnAbbqpmQGjvbpo", "5NMWDxuXmQpNLf7AQ2gj0Y8uBkLXT8q3", CdE0YANGAu4AsDAReO0e6CZ01RwfFe9a][i]
+    ["nvdxlfErdFcQssANvU52VQYNj9JauI4z", "IGE0pfTgoL1OGJKvEcnAbbqpmQGjvbpo", "5NMWDxuXmQpNLf7AQ2gj0Y8uBkLXT8q3", "CdE0YANGAu4AsDAReO0e6CZ01RwfFe9a"][i]
   end
   def self.rescue_api(res, i, url, query, iMAX=1)
     if i == iMAX or res["fault"].nil?
