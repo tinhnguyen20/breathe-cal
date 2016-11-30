f2026423d272b5eed5bfabdf56af35aaf9a7adf6
XIANGJIU Wang <wangxiangjiu1118@gmail.com>
2016-11-30 05:46:36 +0000

    api_working now with newly added api key and put in the front of the array

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+    describe "POST create" do
+        
+    end
+    
+    describe "DELETE #destroy" do
+        it "delete the current client" do
+        response.should redirect_to root_path
+        end
+    end
+
   
 end
