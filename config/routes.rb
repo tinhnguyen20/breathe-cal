4a64d70626ca18ff3cd2e4c48a0edcea7630a2d3
Anish Khazane <akkhazan@berkeley.edu>
2016-12-01 04:19:30 +0000

    favorite cities and un-favorite cities done

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   
   get 'auth/:provider/callback', to: "sessions#create"
 
-  
   get 'authentication_test/:name' => "sessions#create", as: 'auth_test'
 
   get 'auth/failure', to: redirect('/')
