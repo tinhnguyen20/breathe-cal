66f946c68465efe62addf88ba8a3ff5dd59af10a
Alex Beraut <alexberaut@gmail.com>
2017-10-20 00:19:37 +0000

    weather API

--- app/models/city.rb
+++ app/models/city.rb
@@ -18,12 +18,6 @@ class City < ActiveRecord::Base
       response = City.get_resonse(HTTParty.get(url, query: query), url, query)
       self.update_attribute("daily_data" , response)
     end
-    
-    # daily  
-    # daily_data is a hash with key: dailyForecasts value: array of 5 day forecast
-    # we want to get "AirAndPollen" key 
-    # value of AirAndPollen -> array of hashes with Name, Value, Category, CategoryValue, 
-    # the first hash in AirAndPollen also has a type. 
   end
   
   def self.get_loc_key(lat,lng, name)
@@ -38,25 +32,5 @@ class City < ActiveRecord::Base
     City.create(lat: "#{lat}", lng: "#{lng}", location_key: location_key, name: name)
     return location_key
   end
-  
-  # def self.get_location_key(zip, name, state, country)
-  #   city = City.find_by(name: name, state: state,country: country)
-  #   if city
-  #     return city.location_key
-  #   end
-  #   if zip 
-  #     if state and country
-  #       url = "http://dataservice.accuweather.com/locations/v1/#{country}/#{state}/search"
-  #       response = HTTParty.get(url, query: {apikey: "5NMWDxuXmQpNLf7AQ2gj0Y8uBkLXT8q3" ,q: "#{zip}",language:"en-us" } )
-  #       location_key = response[0]["Key"]
-  #       City.create(name: name, zip: zip, state: state, country: country, location_key: location_key )
-  #       return location_key
-  #     end
-  #   end
-  # end
-  
-  
-  
-  
-  
+
 end
