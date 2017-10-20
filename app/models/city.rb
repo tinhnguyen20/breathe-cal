0865deb0d8fe103c91ab472c5c0a7ae2b6710b36
Alex Beraut <alexberaut@gmail.com>
2017-10-20 00:09:22 +0000

    weather API

--- app/models/city.rb
+++ app/models/city.rb
@@ -7,23 +7,33 @@ class City < ActiveRecord::Base
     ["nvdxlfErdFcQssANvU52VQYNj9JauI4z", "IGE0pfTgoL1OGJKvEcnAbbqpmQGjvbpo", "5NMWDxuXmQpNLf7AQ2gj0Y8uBkLXT8q3", "CdE0YANGAu4AsDAReO0e6CZ01RwfFe9a"][i]
   end
   
+  def self.get_accuweather_key()
+    Rails.application.secrets.ACCUWEATHER_KEY
+  end
+  
+  def 
+  
   #THESE ARE GETTING DELETED fa
   def self.rescue_api(res, i, url, query, iMAX=3)
     if i == iMAX or res.code == 200
       return res
     else
-      query[:apikey] = City.get_api_key(i + 1)
+      query[:apikey] = City.get_accuweather_key()
       return City.rescue_api(HTTParty.get(url, query: query), i + 1, url, query)
     end
   end
   
+  def self.get_resonse(resp, url, query)
+    return resp
+      
+      
   def update_city_data
     location_key = self.location_key
     if self.updated_at <= Date.today.to_time.beginning_of_day or !self.daily_data
       url = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/#{location_key}"
-      query = {apikey: City.get_api_key(0), language:"en-us", details: "true"}
-      response = City.rescue_api(HTTParty.get(url, query: query), 0, url, query)
-      self.update_attribute("daily_data" , response) 
+      query = {apikey: City.get_accuweather_key(), language:"en-us", details: "true"}
+      response = City.get_resonse(HTTParty.get(url, query: query), url, query)
+      self.update_attribute("daily_data" , response)
     end
     
     # daily  
@@ -43,7 +53,8 @@ class City < ActiveRecord::Base
       return city.location_key
     end
     url = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search"
-    query = {apikey: City.get_api_key(0) ,q: "#{lat},#{lng}",language:"en-us" }
+    query = {apikey: City.get_accuweather_key(0), q: "#{lat},#{lng}",language:"en-us" }
+    response = City.
     response = City.rescue_api(HTTParty.get(url, query: query), 0, url, query)
     location_key = response["Key"]
     City.create(lat: "#{lat}", lng: "#{lng}", location_key: location_key, name: name)
